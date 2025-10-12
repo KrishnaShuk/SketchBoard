7ef5ff8f02ee125f23d45af724c6655a05e04f43
krishnaShuk <shuklakrishna.kris@gmail.com>
2025-10-12 16:02:11 +0530

    added initial chat page

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,17 @@
+
+interface PropType {
+    placeholder: string;
+    size: "big" | "small"
+}
+
+export default function TextInput({
+    placeholder,
+    size
+}: PropType){
+    return <input placeholder={placeholder} style={{
+        padding: size === "big" ? 20 : 10,
+        margin: size === "big" ? 20 : 10,
+        borderColor:"black",
+        borderWidth: 1,
+    }}></input>
+}
