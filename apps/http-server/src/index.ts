062c7ea79af12900ea7eeeb0fca8b9c411006f24
krishnaShuk <shuklakrishna.kris@gmail.com>
2025-10-14 04:02:06 +0530

    change2

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./consfig.js";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware.js";
+import { CreateUserSchema } from "@repo/common/types"
 
 const app = express();
 
 app.get("/signup", (req, res) => {
-   res.send("hello");
+   const data = CreateUserSchema.safeParse(req.body);
+   if (!data.success) {
+      return res.json({
+         message: "Incorrect inputs"
+      })
+   }
+
+   res.json({
+      userId: "123"
+   })
 })
 
 app.get("/signin", (req, res) => {
