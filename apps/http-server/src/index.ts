33ff508f8c6ed8e7ac5dbe02e57cc252eb88fa49
krishnaShuk <shuklakrishna.kris@gmail.com>
2025-10-16 21:05:31 +0530

    change

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -148,6 +148,6 @@ app.get("/room/:slug", async (req, res) => {
    })
 })
 
-app.listen(3001, () => {
-  console.log('Server listening on port 3001');
+app.listen(3002, () => {
+  console.log('Server listening on port 3002');
 });
